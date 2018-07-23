9724f774815c16de8864fe2f5c082c80511e92f4
liaozhiqiang <liaozhiqiang0102@sailvast.com>
2018-07-23 15:01:08 +0800

    报表实体类

---
+++ src/main/java/com/sv/mc/pojo/ReportParamEntity.java
@@ -0,0 +1,52 @@
+package com.sv.mc.pojo;
+
+/**
+ * 报表接受页面参数实体
+ */
+public class ReportParamEntity {
+    private String startTime;
+    private String endTime;
+    private int provinceId;
+    private int cityId;
+    private int placeId;
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(int placeId) {
+        this.placeId = placeId;
+    }
+}
