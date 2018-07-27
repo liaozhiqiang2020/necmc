0c90330a693f1845574af90ac000f5b0f233b4e5
liaozhiqiang <liaozhiqiang0102@sailvast.com>
2018-07-27 09:36:13 +0800

    菜单和页面导航管理

--- src/main/java/com/sv/mc/controller/DefaultController.java
+++ src/main/java/com/sv/mc/controller/DefaultController.java
@@ -1,13 +1,14 @@
 package com.sv.mc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class DefaultController {
 
-    @GetMapping("/home")
+    @PostMapping("/home")
     public ModelAndView index() {
         return new ModelAndView("/home");
     }
