2b9743b9a46ca821c0c8ed15ecedf583d22f9eb7
87038 <870389491@qq.com>
2018-08-20 16:06:09 +0800

    二维码扫描提示页面

--- src/main/java/com/sv/mc/controller/WXController.java
+++ src/main/java/com/sv/mc/controller/WXController.java
@@ -2,6 +2,7 @@ package com.sv.mc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class WXController {
@@ -12,4 +13,15 @@ public class WXController {
 //    public void turnToTestPage(String sn){
 //        System.out.println(sn);
 //    }
+
+    /**
+     * 跳转到支付宝/微信判断网页
+     */
+    @GetMapping("/wx/wxApp")
+    public ModelAndView toHintPage() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("./error/hint");
+        return mv;
+    }
+
 }
--- src/main/java/com/sv/mc/util/WxUtil.java
+++ src/main/java/com/sv/mc/util/WxUtil.java
@@ -228,4 +228,6 @@ public class WxUtil {
         return sb.toString();
     }
 
+    
+
 }
