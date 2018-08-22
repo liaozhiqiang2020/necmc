da55417abbc145548f7c4111a42ac2fdf2c4e3ab
liaozhiqiang <liaozhiqiang0102@sailvast.com>
2018-08-22 13:40:20 +0800

    1.页面bug修改 2.http改为https

--- src/main/java/com/sv/mc/MCApplication.java
+++ src/main/java/com/sv/mc/MCApplication.java
@@ -1,11 +1,21 @@
 package com.sv.mc;
 
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 @Configuration
@@ -16,8 +26,45 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class MCApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         SpringApplication.run(MCApplication.class, args);
 
     }
+
+//    @Value("${https.port}")
+//    private Integer port;
+//
+//    @Value("${https.ssl.key-store-password}")
+//    private String key_store_password;
+//
+//    @Value("${https.ssl.key-password}")
+//    private String key_password;
+
+    @Bean
+    public ServletWebServerFactory servletContainer(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+        try {
+            File keystore = new ClassPathResource("sample.jks").getFile();
+            connector.setScheme("https");
+            connector.setSecure(true);
+            connector.setPort(8443);
+            protocol.setSSLEnabled(true);
+            protocol.setKeystoreFile(keystore.getAbsolutePath());
+            protocol.setKeystorePass("secret");
+            protocol.setKeyPass("password");
+            return connector;
+        }
+        catch (IOException ex) {
+            throw new IllegalStateException("can't access keystore: [" + "keystore"
+                    + "] or truststore: [" + "keystore" + "]", ex);
+        }
+    }
+
 }
--- src/main/java/com/sv/mc/controller/OrderController.java
+++ src/main/java/com/sv/mc/controller/OrderController.java
@@ -65,7 +65,7 @@ public class OrderController {
     /**
      * 跳转到昨日订单信息页面
      */
-    @GetMapping(value="/turnToOrderMgrByYesterday")
+    @GetMapping(value="/orderMgr/turnToOrderMgrByYesterday")
     public ModelAndView turnToOrderMgrByYesterday(){
         return new ModelAndView("./order/orderMgrYesterday");
     }
